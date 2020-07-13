8627128061fddc8e322dcf3754d49717c46622fd
Jacob Mi <jacobmee@gmail.com>
2020-07-12 21:40:01 -0900

    Return the 4th result for error.

--- RedseaWatcher.py
+++ RedseaWatcher.py
@@ -7,7 +7,9 @@ import numpy as np
 from flask import Flask
 import base64
 import requests
+import json
 
+# Baidu API get token
 def get_token ():
     client_id = 'ctBuA0fMObgMHegCTSKCOKHE'
     client_secret = 'xbl7qH58ayTgc8fDuXqUH1wulQ0UGgsG'
@@ -21,6 +23,7 @@ def get_token ():
 
     return token_key
 
+# Baidu API to recoginize the pic
 def get_words (jpg):
     #request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/general_basic"
     request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic"
@@ -39,87 +42,19 @@ def get_words (jpg):
         num = token_info['words_result_num']
         if num > 0:
             token_key = token_info['words_result']
-            print (token_key)
+            print (str(len(token_key))+":"+json.dumps(token_key))
             return token_key
 
     return ""
 
-app = Flask(__name__)
-
-@app.route("/metrics")
-def metrics():
-
-    camera = PiCamera()
-    camera.start_preview()
-    camera.capture('image.jpg')
-    camera.stop_preview()
-    camera.close()
-
-    # open the "original" image
-    orig_image = cv2.imread('image.jpg')
-    orig_image = cv2.rotate(orig_image, cv2.ROTATE_180)
-
-    ######################
-    ####     ORP      ####
-    ######################
-    # capture the area of the text to "read" by setting "top left" and "right bottom" values in the image
-    left   = 360
-    top    = 770
-    right  = left + 440
-    bottom = top + 200
-
-    JPG = 'code_ORP.jpg'
-    orp_image = orig_image[top:bottom,left:right].copy()
-    orp_image = cv2.cvtColor(orp_image, cv2.COLOR_BGR2GRAY)
-    orp_image = cv2.threshold(orp_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-    orp_image = cv2.morphologyEx(orp_image, cv2.MORPH_OPEN, kernel, iterations=1)
-
-    cv2.imwrite(JPG, orp_image)
-
-    ######################
-    ####     PH       ####
-    ######################
-    left   = 1480
-    top    = 180
-    right  = left + 440
-    bottom = top + 200
-
-    # copy the captured area
-
-    JPG = 'code_ph.jpg'
-    ph_image = orig_image[top:bottom,left:right].copy()
-    ph_image = cv2.cvtColor(ph_image, cv2.COLOR_BGR2GRAY)
-    #ph_image = cv2.GaussianBlur(ph_image, (7,7), 0)
-    ph_image = cv2.threshold(ph_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-    ph_image = cv2.morphologyEx(ph_image, cv2.MORPH_OPEN, kernel, iterations=1)
-    cv2.imwrite(JPG, ph_image)
-
-
-    ######################
-    ##   TEMPERATURE  ####
-    ######################
-    # capture the area of the text to "read" by setting "top left" and "right bottom" values in the image
-    left   = 1480
-    top    = 370
-    right  = left + 440
-    bottom = top + 200
-    JPG = 'code_t.jpg'
-    t_image = orig_image[top:bottom,left:right].copy()
-    t_image = cv2.cvtColor(t_image, cv2.COLOR_BGR2GRAY)
-    t_image = cv2.GaussianBlur(t_image, (9,9), 0)
-    t_image = cv2.threshold(t_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-    cv2.imwrite(JPG, t_image)
-
-    JPG = 'redsea.jpg'
-    vis = np.concatenate((orp_image, ph_image, t_image), axis=0)
-    #vis = np.concatenate((orp_image, ph_image), axis=0)
-    cv2.imwrite(JPG, vis)
-
-    words_result = get_words(JPG)
-    size = len(words_result)
+# To analysis the return values
+def populate (words_result):
     metrics = ""
+
+    size = len(words_result)
+    if (size >= 4 or size <= 0): # Error for reading
+        return metrics
+
     if (size > 0): # ORP data
         ORP = float(words_result[0]['words'])
         if ORP > 200 :
@@ -144,6 +79,93 @@ def metrics():
     elif (size == 0): # Missing PH data
         if PH == 24 or PH == 25 or PH == 26:
             metrics = metrics + "PH \nT {:.1f}".format(PH) + "\n"
+
+    return metrics
+
+
+app = Flask(__name__)
+
+@app.route("/metrics")
+def metrics():
+
+    ######################
+    ####    Camera    ####
+    ######################
+    JPG = 'image.jpg'
+    camera = PiCamera()
+    camera.start_preview()
+    camera.capture(JPG)
+    camera.stop_preview()
+    camera.close()
+
+    # open the "original" image
+    orig_image = cv2.imread(JPG)
+    orig_image = cv2.rotate(orig_image, cv2.ROTATE_180)
+
+    ######################
+    #  Generating  ORP
+    ######################
+    left   = 360
+    top    = 770
+    right  = left + 440
+    bottom = top + 200
+
+    # copy the captured area
+    JPG = 'code_ORP.jpg'
+    orp_image = orig_image[top:bottom,left:right].copy()
+    orp_image = cv2.cvtColor(orp_image, cv2.COLOR_BGR2GRAY)
+    orp_image = cv2.threshold(orp_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+    orp_image = cv2.morphologyEx(orp_image, cv2.MORPH_OPEN, kernel, iterations=1)
+
+    cv2.imwrite(JPG, orp_image)
+
+    ######################
+    #  Generating  PH
+    ######################
+    left   = 1480
+    top    = 180
+    right  = left + 440
+    bottom = top + 200
+
+    # copy the captured area
+    JPG = 'code_ph.jpg'
+    ph_image = orig_image[top:bottom,left:right].copy()
+    ph_image = cv2.cvtColor(ph_image, cv2.COLOR_BGR2GRAY)
+    #ph_image = cv2.GaussianBlur(ph_image, (7,7), 0)
+    ph_image = cv2.threshold(ph_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+    ph_image = cv2.morphologyEx(ph_image, cv2.MORPH_OPEN, kernel, iterations=1)
+    cv2.imwrite(JPG, ph_image)
+
+    ######################
+    #  Generating  T
+    ######################
+    left   = 1480
+    top    = 370
+    right  = left + 440
+    bottom = top + 200
+
+    # copy the captured area
+    JPG = 'code_t.jpg'
+    t_image = orig_image[top:bottom,left:right].copy()
+    t_image = cv2.cvtColor(t_image, cv2.COLOR_BGR2GRAY)
+    t_image = cv2.GaussianBlur(t_image, (9,9), 0)
+    t_image = cv2.threshold(t_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    cv2.imwrite(JPG, t_image)
+
+    ######################
+    # Generating Redsea
+    ######################
+    JPG = 'redsea.jpg'
+    vis = np.concatenate((orp_image, ph_image, t_image), axis=0)
+    #vis = np.concatenate((orp_image, ph_image), axis=0)
+    cv2.imwrite(JPG, vis)
+
+    # Baidu API calls
+    words_result = get_words(JPG)
+    size = len(words_result)
+    metrics = populate(words_result)
 
     return metrics, 200, {'Content-Type': 'text/plain; charset=utf-8'}
 
